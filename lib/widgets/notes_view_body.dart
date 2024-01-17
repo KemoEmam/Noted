9322f9b65b121acd2499bdc84aab108adf6aa0de
Kemo Emam <koko.bose85@gmail.com>
2024-01-17 22:51:14 +0200

    .

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_appbar.dart';
-import 'package:notes_app/widgets/custom_note_item.dart';
 import 'package:notes_app/widgets/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {

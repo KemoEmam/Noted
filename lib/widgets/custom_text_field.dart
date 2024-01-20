cf1ae1518f66745307bfc5733bab95f57488dfda
Kemo Emam <koko.bose85@gmail.com>
2024-01-20 23:33:23 +0200

    create custom text field

---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField(
+      {super.key,
+      required this.borderColor,
+      this.textInputType,
+      this.onChanged,
+      this.obscureText = false,
+      required this.hintText,
+      required this.cursorColor,
+      required this.textColor,
+      required this.focusedBorderColor});
+  final String hintText;
+  final bool? obscureText;
+  final Function(String)? onChanged;
+  final Color borderColor;
+  final Color textColor;
+  final Color focusedBorderColor;
+  final Color cursorColor;
+  final TextInputType? textInputType;
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: cursorColor,
+      keyboardType: textInputType,
+      obscureText: obscureText!,
+      style: TextStyle(color: textColor),
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: const TextStyle(color: Colors.grey),
+        border: borderBuilder(),
+        enabledBorder: borderBuilder(borderColor),
+        focusedBorder: borderBuilder(focusedBorderColor),
+      ),
+    );
+  }
+
+  OutlineInputBorder borderBuilder([color]) {
+    return OutlineInputBorder(
+      borderSide: BorderSide(color: color ?? Colors.white),
+      borderRadius: BorderRadius.circular(8),
+    );
+  }
+}

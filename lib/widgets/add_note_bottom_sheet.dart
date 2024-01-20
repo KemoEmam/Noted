fca86cef7eded4fc33f7236b5042646cdce764dc
Kemo Emam <koko.bose85@gmail.com>
2024-01-20 23:33:06 +0200

    add note bottom sheet

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/constants.dart';
+import 'package:notes_app/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.only(top: 32, right: 24, left: 24),
+      child: Column(
+        children: [
+          CustomTextField(
+              borderColor: Colors.white.withOpacity(.7),
+              hintText: 'Text',
+              cursorColor: kPrimaryColor,
+              focusedBorderColor: kPrimaryColor,
+              textColor: Colors.grey),
+        ],
+      ),
+    );
   }
 }

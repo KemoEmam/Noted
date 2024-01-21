11c499d1fefb12ae18a391d697fcb6c4d2a889bc
Kemo Emam <koko.bose85@gmail.com>
2024-01-21 16:56:26 +0200

    finish note bottom sheet

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -9,24 +9,56 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 32, right: 24, left: 24),
-      child: Column(
-        children: [
-          CustomTextField(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            CustomTextField(
+                borderColor: Colors.white.withOpacity(.7),
+                hintText: 'Title',
+                cursorColor: kPrimaryColor,
+                focusedBorderColor: kPrimaryColor,
+                textColor: Colors.grey),
+            const SizedBox(height: 16),
+            CustomTextField(
               borderColor: Colors.white.withOpacity(.7),
-              hintText: 'Title',
+              hintText: 'Content',
               cursorColor: kPrimaryColor,
               focusedBorderColor: kPrimaryColor,
-              textColor: Colors.grey),
-          const SizedBox(height: 16),
-          CustomTextField(
-            borderColor: Colors.white.withOpacity(.7),
-            hintText: 'Content',
-            cursorColor: kPrimaryColor,
-            focusedBorderColor: kPrimaryColor,
-            textColor: Colors.grey,
-            maxLines: 5,
-          )
-        ],
+              textColor: Colors.grey,
+              maxLines: 5,
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            const CustomButton(),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 60,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: kPrimaryColor,
+      ),
+      child: const Center(
+        child: Text(
+          'Add',
+          style: TextStyle(
+              color: Colors.black, fontSize: 18, fontWeight: FontWeight.w600),
+        ),
       ),
     );
   }
